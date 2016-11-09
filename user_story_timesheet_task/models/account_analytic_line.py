dc0266258681567fcdc3941d5136adb2b098eef8
Luis Felipe Miléo <mileo@kmee.com.br>
2016-11-09 16:31:39 -0200

    [NEW] Module to fix userstory link when timesheet_task is installed

---
+++ user_story_timesheet_task/models/account_analytic_line.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Luis Felipe Mileo <mileo@kmee.com.br> - KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, fields, models, _
+
+
+class AccountAnalyticLine(models.Model):
+
+    _inherit = 'account.analytic.line'
+
+    userstory_id = fields.Many2one(
+        comodel_name='user.story',
+        string='User Story',
+        related='task_id.userstory_id',
+        store=True
+    )
